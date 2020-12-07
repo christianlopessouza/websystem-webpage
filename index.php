f65272bef2be278fdb501f285cf069dea2e87b9b
Christian Lopes <christianlopessouza@gmail.com>
2020-12-07 11:32:08 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+<script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/v4-shims.css">
+<link href="./fontawesome-free-5.14.0-web/css/all.css" rel="stylesheet">
+</head>
+<body>
+
+<?php include 'main/TopNav/topnav.php'?>
+<?php include 'main/Banner/banner.php'?>
+<?php include 'main/Nav/nav.php'?>
+<?php include 'main/NavMobile/navmobile.php'?>
+<?php include 'main/RemoteAcess/remote.php'?>
+<?php include 'main/Services/services.php'?>
+<?php include 'main/Bio/bio.php'?>
+<?php include 'main/Metodology/metodology.php'?>
+<?php include 'main/Blog/blog.php'?>
+<?php include 'main/Ebook/ebook.php'?>
+<?php include 'main/Footer/footer.php'?>
+
+
+</body>
+</html>
